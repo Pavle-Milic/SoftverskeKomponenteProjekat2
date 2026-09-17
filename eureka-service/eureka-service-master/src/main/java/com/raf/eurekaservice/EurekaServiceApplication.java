5875a1917efc9400e241315fad7b8ec481d25233
Pavle  Milic <pmilic14223rn@raf.rs>
2026-09-17 15:09:57 +0200

    ovo funkcionise, potrebno dosta refaktorisanja ali sta da se radi, nemam vremena

--- eureka-service/eureka-service-master/src/main/java/com/raf/eurekaservice/EurekaServiceApplication.java
+++ eureka-service/eureka-service-master/src/main/java/com/raf/eurekaservice/EurekaServiceApplication.java
@@ -1,4 +1,4 @@
-package com.raf.cinemaeurekaservice;
+package com.raf.eurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
